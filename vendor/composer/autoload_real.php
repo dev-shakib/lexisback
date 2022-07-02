1a2f7145f66112e12110448d9c87b093e868a049
dev-shakib <mehedi@lskit.com>
2022-07-02 10:23:53 +0600

    updated composer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -65,16 +65,11 @@ class ComposerAutoloaderInit61dc4ddfd38ef2f1efd7427da103568d
     }
 }
 
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
 function composerRequire61dc4ddfd38ef2f1efd7427da103568d($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
         require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
